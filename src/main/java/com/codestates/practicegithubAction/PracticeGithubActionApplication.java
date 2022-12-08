d7acafb7d62bf55a90327d345058dfcfacaf7f5b
wondo07 <110904783+wondo07@users.noreply.github.com>
2022-12-08 14:40:18 +0900

    action

---
+++ src/main/java/com/codestates/practicegithubAction/PracticeGithubActionApplication.java
@@ -0,0 +1,13 @@
+package com.codestates.practicegithubAction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeGithubActionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeGithubActionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codestates/practicegithubAction/PracticeGithubActionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codestates.practicegithubAction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeGithubActionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
